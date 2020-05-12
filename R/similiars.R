b5810dc9c130e4f44f957428a3e8c87c720af9aa
david sjoberg <dav.sjob@gmail.com>
2020-05-12 16:33:08 +0200

    added superheroes, added case_when_skeleton

--- R/similiars.R
+++ R/similiars.R
@@ -56,7 +56,6 @@ find_string_distance <- function(.s, .t, ignore_case = TRUE, ...) {
 #'
 #' @export
 find_most_similar_string <- function(.s, .t, max_dist = Inf, verbose = TRUE, ignore_case = TRUE, feeling_lucky = FALSE, ...) {
- 
   if(any(!is.character(.t), !is.character(.s))) stop("'.s' and '.t' need to be character vectors")
   .dfs <- find_string_distance(.s, .t, ignore_case = ignore_case)
   .dfs <- purrr::map(.dfs, function(.h) {.h %>% dplyr::filter(.data$string_distance <= max_dist)})
@@ -79,7 +78,6 @@ find_most_similar_string <- function(.s, .t, max_dist = Inf, verbose = TRUE, ign
           return(.x$string[1])
           
        } else{
-         
          if(verbose){
            
            warning(paste0("No single most similar string found for '", 
@@ -88,30 +86,133 @@ find_most_similar_string <- function(.s, .t, max_dist = Inf, verbose = TRUE, ign
                           paste(paste0("'", .x$string, "'"), 
                                 collapse = ", "), 
                           "."))
-           
-           
          }
-         
-        
          return(NA_character_)  
-          
         }
-       
       }
       if( nrow(.x) == 0 ){
         if(verbose){
           warning(paste0("No similar string below threshold found for '", .x$input_string[1], "'. Returning NA.\n"))
-          
         }
-     
-        
         return(NA_character_)
-        
       }
-      
-    
-   
     .x %>% dplyr::pull(string)
   })
   out %>% as.character()
 }
+
+
+#' @title case_when_skeleton
+#'
+#' @description Returns a \code{case_when} from \code{dplyr} to replace .s with .r. You can copy the output and put in the script to avoid manual work.
+#'
+#' @param .s a character vector to be replaced.
+#' @param .r a character vector to replace with.
+#' @param var_name Optional. the name of the variable that should be replaced. If NULL or not changed the call of '.s' is used.
+#'
+#' @examples
+#'
+#' case_when_skeleton(letters[1:10],
+#'                    LETTERS[1:10],
+#'                    var_name = "my_letters")
+#'
+#' @return console output with a case_when skeleton
+#'
+#' @export
+case_when_skeleton <- function(.s, .r, var_name = NULL) {
+  if(is.null(var_name)){
+    .v <- rlang::enquo(.s)
+    .v <- rlang::as_name(.v)
+  } else {
+    .v <- var_name
+  }
+  if(any(!is.character(.s), !is.character(.r), !is.character(.v))) stop("'.s', '.r' and 'var_name' need to be character vectors")
+  if(length(.v) != 1) stop("'var_name' need to be a single character element")
+  if(length(.s) != length(.r)) stop("'.s' and '.r' need to have the same length")
+  .sims <- dplyr::tibble(input_string = .s,
+                         most_similiar = .r,
+                         var = .v) %>% 
+    dplyr::distinct() %>% 
+    tidyr::drop_na() %>% 
+    dplyr::filter(.data$input_string != .data$most_similiar)
+  
+  replacement_lines <- purrr::pmap_chr(.sims, ~{
+    stringr::str_glue("{..3} == \"{..1}\" ~ \"{..2}\",")
+  }) %>% 
+    paste0("    ", .) %>% 
+    paste0(collapse = "\n")
+  
+  stringr::str_c("case_when(\n", 
+        replacement_lines, 
+        paste0("\n  T ~ ", 
+               .v, "\n)")) %>% 
+    cat()
+}
+
+## code to prepare `superheroes` dataset goes here
+# Vector of superheroes
+# Source:
+# Encyclopædia Britannica, inc. 
+# Published March 06, 2019
+# url: https://www.britannica.com/topic/list-of-superheroes-2024795
+
+#' Famous super heroes
+#'
+#' A character vector with 46 super heroes
+#'
+#' @format A character vector with 46 super heroes:
+#' @source \url{https://www.britannica.com/topic/list-of-superheroes-2024795}
+"superheroes"
+
+superheroes <- c(
+  "Ant-Man",
+  "Aquaman",
+  "Asterix",
+  "The Atom",
+  "The Avengers",
+  "Batgirl",
+  "Batman",
+  "Batwoman",
+  "Black Canary",
+  "Black Panther",
+  "Captain America",
+  "Captain Marvel",
+  "Catwoman",
+  "Conan the Barbarian",
+  "Daredevil",
+  "The Defenders",
+  "Doc Savage",
+  "Doctor Strange",
+  "Elektra",
+  "Fantastic Four",
+  "Ghost Rider",
+  "Green Arrow",
+  "Green Lantern",
+  "Guardians of the Galaxy",
+  "Hawkeye",
+  "Hellboy",
+  "Incredible Hulk",
+  "Iron Fist",
+  "Iron Man",
+  "Marvelman",
+  "Robin",
+  "The Rocketeer",
+  "The Shadow",
+  "Spider-Man",
+  "Sub-Mariner",
+  "Supergirl",
+  "Superman",
+  "Teenage Mutant Ninja Turtles",
+  "Thor",
+  "The Wasp",
+  "Watchmen",
+  "Wolverine",
+  "Wonder Woman",
+  "X-Men",
+  "Zatanna",
+  "Zatara"
+)
+
+usethis::use_data(superheroes, overwrite = T)
+
+
